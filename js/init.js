1c9c5b304a716796d419a5e184b64701adf1a67e
PDWPQ <93962838+PDWPQ@users.noreply.github.com>
2021-11-08 22:52:52 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyAJxKC0KEHOtq5yKVvbJcP41WjQfBaVu0s",
+apiKey: "AIzaSyAJxKC0KEHOtq5yKVvbJcP41WjQfBaVu0s",
   authDomain: "autentificacion12-32512.firebaseapp.com",
   projectId: "autentificacion12-32512",
   storageBucket: "autentificacion12-32512.appspot.com",
   messagingSenderId: "384501910789",
   appId: "1:384501910789:web:d37f9a6ccacbd8dc1f7b6c",
   measurementId: "G-C8TBBJDWNM"
-    });
+  };
